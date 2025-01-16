ac06afc78fb75381c6540be880368a3c556f4e6d
Brice Lambson <brice@bricelam.net>
2025-01-15 17:27:57 -0700

    Start over

--- clean.cmd
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-SET MC_DIR=%LOCALAPPDATA%\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang
-REM SET MC_DIR=C:\Users\brice\Downloads\bedrock-server-1.18.0.02
-
-ECHO Cleaning up...
-RMDIR /S /Q %MC_DIR%\development_behavior_packs\brices_world_behaviors
